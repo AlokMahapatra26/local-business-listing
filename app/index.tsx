9a43dca603a4f5596919748f1e7a8a70760acebe
Alok Mahapatra <alokmahapatra2604@gmail.com>
2026-02-01 00:46:27 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>Rohit Mahapatraa</Text>
+    </View>
+  );
+}
